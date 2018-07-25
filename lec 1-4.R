076127504bcb87cb287a8bd6a1645dc44a24eb0b
Hyeonseong Jeon <39935435+Cutz-HS@users.noreply.github.com>
2018-07-25 21:48:27 +0900

    Add lec 1-4

---
+++ lec 1-4.R
@@ -0,0 +1,18 @@
+x <- c(1,2,3,4,6)
+y <- c('1','2','3','4','5')
+z <- c('how', 'are', 'you', '?')
+
+length(x)
+length(y)
+x*y
+#error/ int*str
+
+z
+x
+y
+str(x)
+#data type을 알 수 있는 함수
+str(y)
+str(z)
+
+1+2==3
