1a5989609f24e563f76e5e4eb820cb9c47024a4a
cutz-j <cutz309@gmail.com>
2018-08-06 21:58:51 +0900

    Add p150&p156

---
+++ p156.R
@@ -0,0 +1,9 @@
+# p156
+fuel = data.frame(f1=c("c","d","e","p","r"),
+                  fuel_category=c("CNG","diesel","ethanol E85","premium","regular"),
+                  price=c(2.35, 2.38, 2.11, 2.76, 2.22),
+                  stringsAsFactors=F) ; fuel
+class(fuel$fuel_category)
+
+# factor type은 R에서 명목변수를 의미한다(카테고리: 남1,여2 등)
+# 이러한 명목변수는 범주 level로 표시되며 범주화되지만, 연산이 불가능하다.
